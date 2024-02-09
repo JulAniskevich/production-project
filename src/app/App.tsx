da472e14e91a8795a76507d5d23c2b0a1a3c71d9
Julia Aniskevich <jul.aniskevich@hmail.com>
2024-02-09 14:56:35 +0300

    Add Navbar widget & ui kit component Link

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,16 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Link } from 'react-router-dom';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
